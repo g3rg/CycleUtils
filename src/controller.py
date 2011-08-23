97d30b4519054fc7e1aaa153791a759ae81e0ec7
g3rg <mauve.simian@gmail.com>
2011-08-23 15:13:11 +1000

    Initial project

---
+++ src/controller.py
@@ -0,0 +1,28 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainPage(webapp.RequestHandler):
+    
+    
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+class SpokeTensionCalc(webapp.RequestHandler):
+    
+    def get(self):
+        None
+        
+    def post(self):
+        None
+
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
